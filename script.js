71fcb487485dcae9753bf63d8765662d00ec0ac3
Eld3str0zaCodigo <jose.purizaga@tecsup.edu.pe>
2024-07-11 12:17:00 -0500

    V1 Web
    Primera version de mi Aplicativo Web

---
+++ script.js
@@ -0,0 +1,17 @@
+function addNote(){
+    const noteInput = document.getElementById('noteInput');
+    const noteList = document.getElementById('noteList');
+
+    if(noteInput.value.trim() !== ""){
+        const newNote = document.createElement('li');
+        newNote.textContent = noteInput.value;
+        noteList.appendChild(newNote);
+        noteInput.value = "";
+
+   
+    }
+
+    else{
+        alert("Porfa favor, escriba una nota antes de agregar.")
+    }
+}
